d39bba0ec42b2b8205f4fae6a056960745f53aaf
eqmoura01 <victorlvmora@gmail.com>
2024-07-14 12:54:08 -0300

    fix: Corrige captura de valores dos campos do formulario e adiciona o disabled no botao de salvar caso algum campo nao tenha sido preenchido

--- front/src/app/features/add-pais/add-pais.component.ts
+++ front/src/app/features/add-pais/add-pais.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'add-pais',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatButtonToggleModule, RouterLink, ReactiveFormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './add-pais.component.html',
-  styleUrl: './add-pais.component.scss'
+  styleUrls: ['./add-pais.component.scss']
 })
 export class AddPaisComponent {
 
+  form = new FormGroup({
+    nome: new FormControl<String>('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    sigla: new FormControl<String>('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    gentilico: new FormControl<String>('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+  }); 
+
+  handleSubmit() {
+    console.log(this.form.value);
+  }
 }
