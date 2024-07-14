cbaaea1e60cfee78b8bd833b7d2bb0c32d683196
eqmoura01 <victorlvmora@gmail.com>
2024-07-14 11:26:53 -0300

    refactor: adiciona o Lazy Load ao roteamento. Feito so para aprender, nao vejo necessidade de usar aqui.

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
+import { AddPaisComponent } from './features/add-pais/add-pais.component';
 
-export const routes: Routes = [{
-    path: '',
-    component: ListComponent
-}];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ListComponent
+    },
+    {
+        path: 'add-pais',
+        loadComponent:() => 
+            import('./features/add-pais/add-pais.component').then(m => m.AddPaisComponent)   
+    }
+];
