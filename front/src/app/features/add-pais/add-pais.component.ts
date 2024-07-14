9c162e1a6904c5d71043b1977dd32f8a19931cae
eqmoura01 <victorlvmora@gmail.com>
2024-07-14 11:12:27 -0300

    feat: adicionado a tela de adicionar pais

---
+++ front/src/app/features/add-pais/add-pais.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'add-pais',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-pais.component.html',
+  styleUrl: './add-pais.component.scss'
+})
+export class AddPaisComponent {
+
+}
