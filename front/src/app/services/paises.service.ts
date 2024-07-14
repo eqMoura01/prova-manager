64e9f21b71ecdb9546db79a01b4f8556ed8db890
eqmoura01 <victorlvmora@gmail.com>
2024-07-14 11:06:04 -0300

    fix: refatoracao da classe paises.service

--- front/src/app/services/paises.service.ts
+++ front/src/app/services/paises.service.ts
@@ -1,12 +1,12 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Pais } from '../interfaces/pais.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisesService {
-  httpClient = inject(HttpClient);
+  constructor(private httpClient: HttpClient) {}
 
   listAll() {
     return this.httpClient.get<Pais[]>('/api/paises');
