222a8f92ec93bd5e6e766cbb387baf6b2c2eebb6
eqmoura01 <victorlvmora@gmail.com>
2024-07-16 01:23:24 -0300

    fix: remove implementacao de cadastro de usuarios

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { CadastroUsuarioComponent } from './features/cadastro-usuario/cadastro-usuario.component';
 import { ListComponent } from './features/list/list.component';
 import { LoginComponent } from './features/login/login.component';
 import { PaisResolver } from './resolvers/PaisResolver';
@@ -8,10 +7,6 @@ export const routes: Routes = [
     {
         path: '',
         component: LoginComponent
-    },
-    {
-        path: 'cadastro-usuario',
-        component: CadastroUsuarioComponent
     },
     {
         path: 'home',
