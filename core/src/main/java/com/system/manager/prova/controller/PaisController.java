ffebf2ffe76f7bcce5db907193af38ab86888193
eqmoura01 <victorlvmora@gmail.com>
2024-07-16 03:13:25 -0300

    fix: corrige o endpoint /deletar/id

--- core/src/main/java/com/system/manager/prova/controller/PaisController.java
+++ core/src/main/java/com/system/manager/prova/controller/PaisController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,7 +40,7 @@ public class PaisController {
     }
 
     @DeleteMapping("/deletar/{id}")
-    public ResponseEntity<Pais> deletar(@RequestBody Long id) {
+    public ResponseEntity<Pais> deletar(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(this.paisService.deleteById(id));
     }
 }
