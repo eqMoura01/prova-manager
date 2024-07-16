53ffd6ae1584217dd5c1ab3a6d0bf458156ecea4
eqmoura01 <victorlvmora@gmail.com>
2024-07-16 01:34:16 -0300

    feat: adiciona listagem de paises atraves de comunicacao com o backend

--- front/src/app/features/list/list.component.ts
+++ front/src/app/features/list/list.component.ts
@@ -1,29 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { Router, RouterLink } from '@angular/router';
-import { Pais } from '../../interfaces/pais.interface';
-import { PaisesService } from '../../services/paises.service';
 import { AppHeaderComponent } from "../../components/app-header/app-header.component";
-import { CommonModule } from '@angular/common';
+import { Pais } from '../../interfaces/pais.interface';
+import { ApiService } from '../../services/api.service';
+import { PaisesService } from '../../services/paises.service';
 
 @Component({
   selector: 'app-list',
   standalone: true,
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
-  imports: [MatTableModule, RouterLink, AppHeaderComponent, AppHeaderComponent, CommonModule]
+  imports: [MatTableModule, RouterLink, AppHeaderComponent, AppHeaderComponent, CommonModule],
+  providers: [
+    ApiService,
+    { provide: 'BASE_URL', useValue: 'http://localhost:8080' }
+  ]
 })
 export class ListComponent implements OnInit {
-  
+
   isAdministrador: boolean = false;
   matSnackBar = inject(MatSnackBar);
-  
+
   displayedColumns: string[] = ['Sigla', 'País', 'Gentilico'];
 
   dataSource: Pais[] = [];
 
-  constructor(private paisesService: PaisesService, private router: Router) { }
+  constructor(private paisesService: PaisesService, private router: Router, private api: ApiService) { 
+    this.api = new ApiService('http://localhost:8080/')
+  }
 
   ngOnInit() {
     this.isAdministrador = JSON.parse(localStorage.getItem('user') ?? '').isAdministrador;
@@ -32,9 +39,16 @@ export class ListComponent implements OnInit {
       this.displayedColumns.push('Acoes');
     }
 
-    this.paisesService.listAll().subscribe(paises => (
-      this.dataSource = paises
-    ));
+    this.api.get('pais/listar').then(response => {
+      this.dataSource = response
+    }).catch(err => {
+      console.error(err)
+          this.matSnackBar.open('Não foi possivel listar os países! Verifique sua conexao com o servidor.', 'Fechar', {
+          duration: 3000,
+          horizontalPosition: 'right',
+          verticalPosition: 'bottom',
+        });
+      });
   }
 
   handleUpdate(pais: Pais) {
