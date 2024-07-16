df82e97e61b36c90e1411d24ef6fb24b1d0058a7
eqmoura01 <victorlvmora@gmail.com>
2024-07-16 01:57:38 -0300

    feat: implementa validacao de token no cadastro de pais

--- front/src/app/features/add-pais/add-pais.component.ts
+++ front/src/app/features/add-pais/add-pais.component.ts
@@ -6,15 +6,20 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router, RouterLink } from '@angular/router';
-import { PaisesService } from '../../services/paises.service';
 import { AppHeaderComponent } from '../../components/app-header/app-header.component';
+import { ApiService } from '../../services/api.service';
+import { PaisesService } from '../../services/paises.service';
 
 @Component({
   selector: 'add-pais',
   standalone: true,
   imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatButtonToggleModule, RouterLink, ReactiveFormsModule, MatFormFieldModule, MatInputModule, AppHeaderComponent],
   templateUrl: './add-pais.component.html',
-  styleUrls: ['./add-pais.component.scss']
+  styleUrls: ['./add-pais.component.scss'],
+  providers: [
+    ApiService,
+    { provide: 'BASE_URL', useValue: 'http://localhost:8080' }
+  ]
 })
 export class AddPaisComponent {
 
@@ -33,25 +38,90 @@ export class AddPaisComponent {
     }),
   });
 
-  constructor(private paisesService: PaisesService, private router: Router) { }
+  constructor(private paisesService: PaisesService, private router: Router, private api: ApiService) { }
 
   matSnackBar = inject(MatSnackBar);
+  valid: boolean = false;
+
 
   handleSubmit() {
-    this.paisesService.save({
-      nome: this.form.value.nome ?? '',
-      sigla: this.form.value.sigla ?? '',
-      gentilico: this.form.value.gentilico ?? '',
-    }).subscribe(() => {
-      this.form.reset();
-      this.router.navigate(['/home']);
 
-      this.matSnackBar.open('País cadastrado com sucesso!', 'Fechar', {
-        duration: 3000,
-        horizontalPosition: 'right',
-        verticalPosition: 'bottom',
+    this.api.get('usuario/renovar-ticket', {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('jwt')}`
+      }
+    })
+      .then(response => {
+        this.valid = response;
+        if (this.valid) {
+          this.api.post('pais/salvar', {
+            nome: this.form.value.nome ?? '',
+            sigla: this.form.value.sigla ?? '',
+            gentilico: this.form.value.gentilico ?? '',
+          }, {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem('jwt')}`
+            }
+          }).then(response => {
+            this.form.reset();
+            this.router.navigate(['/home']);
+
+            this.matSnackBar.open('País cadastrado com sucesso!', 'Fechar', {
+              duration: 3000,
+              horizontalPosition: 'right',
+              verticalPosition: 'bottom',
+            });
+          }).catch(err => {
+            console.error(err)
+            this.matSnackBar.open('Não foi possivel cadastrar o país! Verifique sua conexao com o servidor.', 'Fechar', {
+              duration: 3000,
+              horizontalPosition: 'right',
+              verticalPosition: 'bottom',
+            });
+            this.router.navigate(['/home']);
+          }
+          )
+        } else {
+          this.matSnackBar.open('Sua sessão expirou! Faça login novamente.', 'Fechar', {
+            duration: 3000,
+            horizontalPosition: 'right',
+            verticalPosition: 'bottom',
+          });
+
+          this.router.navigate(['/']);
+        }
+      })
+      .catch(err => {
+        this.valid = false
+        console.error(err)
+        this.matSnackBar.open('Sua sessão expirou! Faça login novamente.', 'Fechar', {
+          duration: 3000,
+          horizontalPosition: 'right',
+          verticalPosition: 'bottom',
+        });
+
+        this.router.navigate(['/']);
       });
 
-    });
+
+
+
+
+
+    // this.paisesService.save({
+    //   nome: this.form.value.nome ?? '',
+    //   sigla: this.form.value.sigla ?? '',
+    //   gentilico: this.form.value.gentilico ?? '',
+    // }).subscribe(() => {
+    //   this.form.reset();
+    //   this.router.navigate(['/home']);
+
+    //   this.matSnackBar.open('País cadastrado com sucesso!', 'Fechar', {
+    //     duration: 3000,
+    //     horizontalPosition: 'right',
+    //     verticalPosition: 'bottom',
+    //   });
+
+    // });
   }
 }
